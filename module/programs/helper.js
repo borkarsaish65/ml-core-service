b331055050dbb815ab3a5d153c5ba33f793b2d1f
borkarsaish65 <borkarsaish65@gmail.com>
2025-01-03 16:13:46 +0530

    savepoint

--- module/programs/helper.js
+++ module/programs/helper.js
@@ -228,93 +228,97 @@ module.exports = class ProgramsHelper {
         }
 
         let scope = {};
-
-        if( scopeData.entityType ) {
-          // Get entity details of type {scopeData.entityType}
-          let bodyData = {
-            "type" : scopeData.entityType
-          }
-          let entityTypeData = await userService.locationSearch( bodyData );
-          
-          if( !entityTypeData.success ) {
-            return resolve({
-              status : httpStatusCode.bad_request.status,
-              message : constants.apiResponses.ENTITY_TYPES_NOT_FOUND
-            });
-          }
-
-          scope["entityType"] = entityTypeData.data[0].type;
-  
-        }
-
-        if( scopeData.entities && scopeData.entities.length > 0 ) {
-          
-          //call learners api for search
-          let entityIds = [];
-          let bodyData={};
-          let locationData = gen.utils.filterLocationIdandCode(scopeData.entities)
-          
-          //locationIds contain id of location data. 
-          if ( locationData.ids.length > 0 ) {
-            bodyData = {
-              "id" : locationData.ids,
-              "type" : scopeData.entityType
-            } 
-            let entityData = await userService.locationSearch( bodyData );
-            if ( entityData.success ) {
-              entityData.data.forEach( entity => {
-                entityIds.push(entity.id)
-              });
-            }
-          }
-          
-          if ( locationData.codes.length > 0 ) {
-            let filterData = {
-              "code" : locationData.codes,
+        if(false){
+          if( scopeData.entityType ) {
+            // Get entity details of type {scopeData.entityType}
+            let bodyData = {
               "type" : scopeData.entityType
             }
-            let entityDetails = await userService.locationSearch( filterData );
+            let entityTypeData = await userService.locationSearch( bodyData );
             
-            if ( entityDetails.success ) {
-              let entitiesData = entityDetails.data;
-              entitiesData.forEach( entity => {
-                entityIds.push(entity.id) 
-              });
-            }
-          }
-          
-          if( !entityIds.length > 0 ) {
-              throw {
-                message : constants.apiResponses.ENTITIES_NOT_FOUND
-              };
-          }
-          scope["entities"] = entityIds;
-        } 
-
-        if( scopeData.roles ) {
-          
-          if( Array.isArray(scopeData.roles) && scopeData.roles.length > 0 ) {
-            
-            let userRoles = await userRolesHelper.roleDocuments({
-              code : { $in : scopeData.roles }
-            },["_id","code"]);
-            
-            if( !userRoles.length > 0 ) {
+            if( !entityTypeData.success ) {
               return resolve({
                 status : httpStatusCode.bad_request.status,
-                message : constants.apiResponses.INVALID_ROLE_CODE
+                message : constants.apiResponses.ENTITY_TYPES_NOT_FOUND
               });
             }
+  
+            scope["entityType"] = entityTypeData.data[0].type;
     
-            scope["roles"] = userRoles;
-          } else {
-            if( scopeData.roles === constants.common.ALL_ROLES ) {
-              scope["roles"] = [{
-                "code" : constants.common.ALL_ROLES
-              }]; 
+          }
+  
+          if( scopeData.entities && scopeData.entities.length > 0 ) {
+            
+            //call learners api for search
+            let entityIds = [];
+            let bodyData={};
+            let locationData = gen.utils.filterLocationIdandCode(scopeData.entities)
+            
+            //locationIds contain id of location data. 
+            if ( locationData.ids.length > 0 ) {
+              bodyData = {
+                "id" : locationData.ids,
+                "type" : scopeData.entityType
+              } 
+              let entityData = await userService.locationSearch( bodyData );
+              if ( entityData.success ) {
+                entityData.data.forEach( entity => {
+                  entityIds.push(entity.id)
+                });
+              }
+            }
+            
+            if ( locationData.codes.length > 0 ) {
+              let filterData = {
+                "code" : locationData.codes,
+                "type" : scopeData.entityType
+              }
+              let entityDetails = await userService.locationSearch( filterData );
+              
+              if ( entityDetails.success ) {
+                let entitiesData = entityDetails.data;
+                entitiesData.forEach( entity => {
+                  entityIds.push(entity.id) 
+                });
+              }
+            }
+            
+            if( !entityIds.length > 0 ) {
+                throw {
+                  message : constants.apiResponses.ENTITIES_NOT_FOUND
+                };
+            }
+            scope["entities"] = entityIds;
+          } 
+  
+          if( scopeData.roles ) {
+            
+            if( Array.isArray(scopeData.roles) && scopeData.roles.length > 0 ) {
+              
+              let userRoles = await userRolesHelper.roleDocuments({
+                code : { $in : scopeData.roles }
+              },["_id","code"]);
+              
+              if( !userRoles.length > 0 ) {
+                return resolve({
+                  status : httpStatusCode.bad_request.status,
+                  message : constants.apiResponses.INVALID_ROLE_CODE
+                });
+              }
+      
+              scope["roles"] = userRoles;
+            } else {
+              if( scopeData.roles === constants.common.ALL_ROLES ) {
+                scope["roles"] = [{
+                  "code" : constants.common.ALL_ROLES
+                }]; 
+              }
             }
           }
+        }else{
+          scope = scopeData;
         }
+
 
         let updateProgram = 
         await database.models.programs.findOneAndUpdate(
